d15dc62b1c702b26e93728a5bf41430e51784890
Yonca RAY <rayyonca@gmail.com>
2021-02-11 03:22:02 +0300

    Add project files.

---
+++ TabanDonusturme/frmTabanDonusturme.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace TabanDonusturme
+{
+    partial class frmTabanDonusturme
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmTabanDonusturme));
+            this.panelControl1 = new DevExpress.XtraEditors.PanelControl();
+            this.btnDonustur = new DevExpress.XtraEditors.SimpleButton();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtSonuc = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSayi = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbDonusturulecekTaban = new System.Windows.Forms.ComboBox();
+            this.cmbAsilTaban = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).BeginInit();
+            this.panelControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelControl1
+            // 
+            this.panelControl1.Appearance.BackColor = System.Drawing.Color.SteelBlue;
+            this.panelControl1.Appearance.BackColor2 = System.Drawing.Color.SkyBlue;
+            this.panelControl1.Appearance.Options.UseBackColor = true;
+            this.panelControl1.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
+            this.panelControl1.Controls.Add(this.btnDonustur);
+            this.panelControl1.Controls.Add(this.label6);
+            this.panelControl1.Controls.Add(this.label4);
+            this.panelControl1.Controls.Add(this.txtSonuc);
+            this.panelControl1.Controls.Add(this.label3);
+            this.panelControl1.Controls.Add(this.txtSayi);
+            this.panelControl1.Controls.Add(this.label2);
+            this.panelControl1.Controls.Add(this.label1);
+            this.panelControl1.Controls.Add(this.cmbDonusturulecekTaban);
+            this.panelControl1.Controls.Add(this.cmbAsilTaban);
+            this.panelControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelControl1.Location = new System.Drawing.Point(0, 0);
+            this.panelControl1.Name = "panelControl1";
+            this.panelControl1.Size = new System.Drawing.Size(534, 341);
+            this.panelControl1.TabIndex = 0;
+            // 
+            // btnDonustur
+            // 
+            this.btnDonustur.Appearance.Font = new System.Drawing.Font("Elephant", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDonustur.Appearance.Options.UseFont = true;
+            this.btnDonustur.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btnDonustur.ImageOptions.Image")));
+            this.btnDonustur.Location = new System.Drawing.Point(249, 193);
+            this.btnDonustur.Name = "btnDonustur";
+            this.btnDonustur.Size = new System.Drawing.Size(182, 35);
+            this.btnDonustur.TabIndex = 31;
+            this.btnDonustur.Text = "DÖNÜŞTÜR";
+            this.btnDonustur.Click += new System.EventHandler(this.btnDonustur_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(244, 193);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 25);
+            this.label6.TabIndex = 30;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(178, 258);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 25);
+            this.label4.TabIndex = 29;
+            this.label4.Text = "Sonuç:";
+            // 
+            // txtSonuc
+            // 
+            this.txtSonuc.Enabled = false;
+            this.txtSonuc.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSonuc.Location = new System.Drawing.Point(249, 255);
+            this.txtSonuc.Name = "txtSonuc";
+            this.txtSonuc.Size = new System.Drawing.Size(182, 30);
+            this.txtSonuc.TabIndex = 28;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(195, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 25);
+            this.label3.TabIndex = 27;
+            this.label3.Text = "Sayı:";
+            // 
+            // txtSayi
+            // 
+            this.txtSayi.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSayi.Location = new System.Drawing.Point(249, 140);
+            this.txtSayi.Name = "txtSayi";
+            this.txtSayi.Size = new System.Drawing.Size(182, 30);
+            this.txtSayi.TabIndex = 26;
+            this.txtSayi.TextChanged += new System.EventHandler(this.txtSayi_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(49, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(237, 25);
+            this.label2.TabIndex = 25;
+            this.label2.Text = "Dönüştürülecek Taban:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(146, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 25);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "Asıl Taban:";
+            // 
+            // cmbDonusturulecekTaban
+            // 
+            this.cmbDonusturulecekTaban.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cmbDonusturulecekTaban.FormattingEnabled = true;
+            this.cmbDonusturulecekTaban.Items.AddRange(new object[] {
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16"});
+            this.cmbDonusturulecekTaban.Location = new System.Drawing.Point(249, 99);
+            this.cmbDonusturulecekTaban.Name = "cmbDonusturulecekTaban";
+            this.cmbDonusturulecekTaban.Size = new System.Drawing.Size(182, 33);
+            this.cmbDonusturulecekTaban.TabIndex = 23;
+            // 
+            // cmbAsilTaban
+            // 
+            this.cmbAsilTaban.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cmbAsilTaban.FormattingEnabled = true;
+            this.cmbAsilTaban.Items.AddRange(new object[] {
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16"});
+            this.cmbAsilTaban.Location = new System.Drawing.Point(249, 58);
+            this.cmbAsilTaban.Name = "cmbAsilTaban";
+            this.cmbAsilTaban.Size = new System.Drawing.Size(182, 33);
+            this.cmbAsilTaban.TabIndex = 22;
+            // 
+            // frmTabanDonusturme
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(534, 341);
+            this.Controls.Add(this.panelControl1);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "frmTabanDonusturme";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TABAN DÖNÜŞTÜRME PROGRAMI";
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).EndInit();
+            this.panelControl1.ResumeLayout(false);
+            this.panelControl1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.PanelControl panelControl1;
+        private DevExpress.XtraEditors.SimpleButton btnDonustur;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtSonuc;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSayi;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbDonusturulecekTaban;
+        private System.Windows.Forms.ComboBox cmbAsilTaban;
+    }
+}
+
---
+++ TabanDonusturme/frmTabanDonusturme.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TabanDonusturme
+{
+    public partial class frmTabanDonusturme : Form
+    {
+        public frmTabanDonusturme()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDonustur_Click(object sender, EventArgs e)
+        {
+            string asilTaban = cmbAsilTaban.Text;
+            string donusturulecekTaban = cmbDonusturulecekTaban.Text;
+            string sayi = txtSayi.Text;
+            string[] dizi = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F" };
+
+            if (cmbAsilTaban.Text == "" || cmbDonusturulecekTaban.Text == "" || txtSayi.Text == "") MessageBox.Show("Lütfen alanları doldurun!", "UYARI", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            else
+            {
+                if (tabanKontrol(sayi, asilTaban, dizi) == true) MessageBox.Show("Sayı " + asilTaban + " tabanında değildir!", "UYARI", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                else
+                {
+                    if (donusturulecekTaban == "10") txtSonuc.Text = onluTabanaCevirme(sayi, asilTaban, dizi);
+                    else
+                    {
+                        sayi = onluTabanaCevirme(sayi, asilTaban, dizi);
+                        txtSonuc.Text = onluTabandanCevirme(sayi, donusturulecekTaban, dizi);
+                    }
+                }
+            }
+                        
+        }
+
+        static string onluTabanaCevirme(string sayi, string asilTaban, string[] dizi)
+        {
+            double toplam = 0;
+            int sayac = sayi.Length - 1;
+            for (int i = 0; i < sayi.Length; i++)
+            {
+                int index = Array.IndexOf(dizi, sayi[i].ToString());
+                toplam += index * Math.Pow(Convert.ToUInt32(asilTaban), sayac);
+                sayac--;
+            }
+            string sonuc = toplam.ToString();
+            return sonuc;
+        }
+      
+        static string onluTabandanCevirme(string sayi, string donusturulecekTaban, string[] dizi)
+        {
+            int kalan;
+            string sonuc = "";
+            int yedekSayi = int.Parse(sayi);
+            int bolen = int.Parse(donusturulecekTaban);
+            while (yedekSayi > 0)
+            {
+                kalan = yedekSayi % bolen;
+                sonuc = dizi[kalan].ToString() + sonuc;
+                yedekSayi /= bolen;
+            }
+            return sonuc;
+        }
+
+        static bool tabanKontrol(string sayi, string asilTaban, string[] dizi)
+        {
+            bool sonuc = false;
+            for (int i = 0; i < sayi.Length; i++)
+            {
+                int index = Array.IndexOf(dizi, sayi[i].ToString());
+                if (index >= int.Parse(asilTaban))
+                {
+                    sonuc = true;
+                    break;
+                }
+            }
+            return sonuc;
+        }
+
+        private void txtSayi_TextChanged(object sender, EventArgs e)
+        {
+            txtSayi.Text = txtSayi.Text.ToUpper();
+            txtSayi.SelectionStart = txtSayi.Text.Length;
+        }
+    }
+}
